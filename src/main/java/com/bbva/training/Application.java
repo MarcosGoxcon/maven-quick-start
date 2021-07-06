ce7eed186dc12ad204e62816c1c8edd15e16abaf
MarcosGoxconPortillo <marcosgoxconportillo@gmail.com>
2021-07-06 16:33:13 -0500

    agregando dependencia

--- src/main/java/com/bbva/training/Application.java
+++ src/main/java/com/bbva/training/Application.java
@@ -3,6 +3,7 @@ package com.bbva.training;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
@@ -19,10 +20,17 @@ public class Application {
 
     }
 
+    public static int countWords(String words){
+        String [] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null)  ? 0 : separateWords.length;
+    }
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
     app.greet();
+    int count = countWords("I have four words");
+    System.out.println(count);
     }
 }
